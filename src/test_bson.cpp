0ebca59981185799d7e274eeff7a86217ae678c1
qiuxinyun2010 <877368536@qq.com>
2023-03-09 15:30:37 +0800

    update

--- src/test_bson.cpp
+++ src/test_bson.cpp
@@ -1,14 +1,22 @@
 #include "builder.h"
 using namespace std;
+
+void f(int* x){
+    cout<<x<<endl;
+}
 int test_bson_main() {
-    quadis::BufBuilder buf_builder;
-    cout<< sizeof(buf_builder)<<endl;
-    printf("%p\n", buf_builder.buf());
-    char* p = buf_builder.buf();
-    buf_builder.grow(1024);
-    // buf_builder.appendNum(4);
-    printf("%p %p\n", buf_builder.buf(), p);
-    // printf("%d\n",*(int*)(p));
-    buf_builder.kill();
+    // quadis::BufBuilder buf_builder;
+    // cout<< sizeof(buf_builder)<<endl;
+    // printf("%p\n", buf_builder.buf());
+    // char* p = buf_builder.buf();
+    // buf_builder.grow(1024);
+    // // buf_builder.appendNum(4);
+    // printf("%p %p\n", buf_builder.buf(), p);
+    // // printf("%d\n",*(int*)(p));
+    // buf_builder.kill();
+    int a = 99;
+    int &b = a;
+    f(&a);
+    f(&b);
     return 1;
 }
